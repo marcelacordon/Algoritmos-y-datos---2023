b8a4d6481da30d38bdffe94414188c68c1c1b068
DiegoDuaS <110642453+DiegoDuaS@users.noreply.github.com>
2023-02-10 11:34:07 -0600

    Cambio 1
    ----

--- ejercicio1/Main.java
+++ ejercicio1/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Animal dog = AnimalFactory.getAnimal("dog", "Max");
+        Animal dog = AnimalFactory.getAnimal("dog", "Toby");
         dog.speak();
 
-        Animal cat = AnimalFactory.getAnimal("cat", "Luna");
+        Animal cat = AnimalFactory.getAnimal("cat", "Dallas");
         cat.speak();
     }
 }
