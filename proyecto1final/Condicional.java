1df6d246c14de0499a2c7f7098a5cfa824f61b67
DiegoDuaS <110642453+DiegoDuaS@users.noreply.github.com>
2023-03-16 17:49:13 -0600

    Condicionales

---
+++ proyecto1final/Condicional.java
@@ -0,0 +1,13 @@
+public class Condicional {
+    Predicados p = new Predicados();
+
+    public String cond(String input){
+        int i = 0;
+        input = input.substring(1, input.length() - 1);
+        String[] tokens = input.split("\\s+");
+        while (i<tokens.length){
+        }
+
+    }
+
+}
